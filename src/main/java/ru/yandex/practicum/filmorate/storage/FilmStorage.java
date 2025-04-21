54f5a1aa315ccec8e4893d033c2c460d3968ee37
Maxim Utkin <maxim-utkin-disk@yandex.ru>
2025-04-21 11:30:52 +0300

    Filmorate 2: стилевые правки ГХ #4

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -4,9 +4,9 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.ArrayList;
 
-public interface FilmStorage   {
+public interface FilmStorage {
     Film addNewFilm(Film newFilm);
-    Film updateExistingFilm (Film updFilm);
+    Film updateExistingFilm(Film updFilm);
     Film getFilmById(int filmId);
     ArrayList<Film> getFilmsList();
 }
