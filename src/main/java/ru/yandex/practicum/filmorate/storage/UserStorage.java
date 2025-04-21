89788d1905c6feaee565e4fd910aee5319f42dd0
Maxim Utkin <maxim-utkin-disk@yandex.ru>
2025-04-21 11:32:25 +0300

    Filmorate 2: стилевые правки ГХ #5

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,8 +5,13 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.ArrayList;
 
 public interface UserStorage {
+
     User createNewUser(User newUser);
+
     User updateExistingUser(User updUser);
+
     User getUserById(int userId);
+
     ArrayList<User> getUsersList();
+
 }
